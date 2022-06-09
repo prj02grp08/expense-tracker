d95c10acac3ca18b6afe4b8d4283bbf4cb5c12c0
katochsenthal <katoch.senthal@gmail.com>
2022-06-08 23:11:49 -0400

    added graph using chartJs

--- public/javascript/expense.js
+++ public/javascript/expense.js
@@ -1,26 +1,27 @@
 async function addExpenses() {
-    const expenseName = document.querySelector('#new-expense').value.trim();
-    const expenseValue = document.querySelector('#new-expense-value').value.trim();
-    const user_id = document.querySelector('#user_id').value;
-    const category_id = document.querySelector('#select-category').value;
+  const expenseName = document.querySelector("#new-expense").value.trim();
+  const expenseValue = document
+    .querySelector("#new-expense-value")
+    .value.trim();
+  const user_id = document.querySelector("#user_id").value;
+  const category_id = document.querySelector("#select-category").value;
 
-    console.log("this", expenseName, expenseValue, user_id, category_id);
-    // const expenseName = document.querySelector('#new-expense').value.trim();
+  console.log("this", expenseName, expenseValue, user_id, category_id);
+  // const expenseName = document.querySelector('#new-expense').value.trim();
 
-    const response = await fetch('/api/expenses', {
-        method: 'POST',
-        body: JSON.stringify({
-            name: expenseName,
-            value: expenseValue,
-            user_id: user_id,
-            category_id: category_id
-        }),
-        headers: { 'Content-Type': 'application/json' }
-    });
-    if (response.ok) {
-        console.log(response)
-    }
+  const response = await fetch("/api/expenses", {
+    method: "POST",
+    body: JSON.stringify({
+      name: expenseName,
+      value: expenseValue,
+      user_id: user_id,
+      category_id: category_id,
+    }),
+    headers: { "Content-Type": "application/json" },
+  });
+  if (response.ok) {
+    console.log(response);
+  }
 }
 
-document.querySelector('.expense-form').addEventListener('submit', addExpenses);
-
+document.querySelector(".expense-form").addEventListener("submit", addExpenses);
--- public/javascript/reports.js
+++ public/javascript/reports.js
@@ -20,6 +20,52 @@
 
 // }
 
-
-
 // document.querySelector('.view-transaction').addEventListener('submit', getSomething);
+// document.querySelector('.generate-graph').addEventListener('submit', import google graph);
+
+// import { GoogleCharts } from "google-charts";
+
+const generateGraph = async () => {
+  const res = await fetch("http://localhost:3001/api/expenses");
+  const data = await res.json();
+  const names = data.map((item) => item.name);
+  const values = data.map((item) => item.value);
+
+  const ctx = document.getElementById("myChart").getContext("2d");
+  const myChart = new Chart(ctx, {
+    type: "doughnut",
+    data: {
+      labels: names,
+      datasets: [
+        {
+          label: "My First Dataset",
+          data: values,
+          backgroundColor: [
+            "rgb(255, 99, 132)",
+            "rgb(54, 162, 235)",
+            "rgb(255, 205, 86)",
+          ],
+          hoverOffset: 4,
+        },
+      ],
+    },
+  });
+};
+
+window.generateGraph = generateGraph;
+//Load the charts library with a callback
+// GoogleCharts.load(drawChart);
+
+// function drawChart() {
+//   // Standard google charts functionality is available as GoogleCharts.api after load
+//   const data = GoogleCharts.api.visualization.arrayToDataTable([
+//     ["Chart thing", "Chart amount"],
+//     ["Lorem ipsum", 60],
+//     ["Dolor sit", 22],
+//     ["Sit amet", 18],
+//   ]);
+//   const pie_1_chart = new GoogleCharts.api.visualization.PieChart(
+//     document.getElementById("piechart_3d")
+//   );
+//   pie_1_chart.draw(data);
+// }
